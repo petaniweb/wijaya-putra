54ed8fab53534dbac14e43cc4155f621c6bfb6ae
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2024-05-15 14:07:55 +0700

    feat : Contact Us Page Desktop created

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+
+// Import Components //
+import Navbar from "@/components/navbar";
+import ContactForm from "@/components/contact-form";
+import Footer from "@/components/footer";
+
+export default function Contact() {
+	return (
+		<>
+			{/* <-- ==== Navbar Start ==== --> */}
+			<Navbar />
+			{/* <-- ==== Navbar End ==== --> */}
+
+			{/* <-- ==== Contact Us Start ==== --> */}
+			<section className="w-full bg-cover bg-center bg-bgcontact h-auto pt-36 pb-20 px-sectionpxlg 2xl:px-sectionpx2xl">
+				<div className="w-full flex items-center justify-end">
+					<ContactForm />
+				</div>
+			</section>
+			{/* <-- ==== Contact Us End ==== --> */}
+
+			{/* <-- ==== Footer Start ==== --> */}
+			<Footer />
+			{/* <-- ==== Footer End ==== --> */}
+		</>
+	);
+}
